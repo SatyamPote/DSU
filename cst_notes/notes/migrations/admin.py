716838b802c949a506488abc64d5d3b7b815fb7e
ahmedatk <ahmedzaweel@gmail.com>
2025-02-22 21:49:11 +0530

    file structure

--- cst_notes/notes/migrations/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Note
-
-@admin.register(Note)
-class NoteAdmin(admin.ModelAdmin):
-    list_display = ('title', 'category', 'uploaded_by', 'upload_date')
-    search_fields = ('title', 'uploaded_by')
-    list_filter = ('category', 'upload_date')
--- cst_notes/notes/migrations/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import home, notes
-
-urlpatterns = [
-    path('', home, name='home'),
-    path('notes/', notes, name='notes'),
-]
