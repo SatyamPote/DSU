25b5bdd4b2a0457703af5dfcb1099d8629ade797
Satyam <satyampote9999@gmail.com>
2025-03-01 16:27:49 +0530

    admin
    Cst@123

--- CST/CST/settings.py
+++ CST/CST/settings.py
@@ -86,7 +86,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Verify this path
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
