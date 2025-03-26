6c6e398619fb9ab5360399c1af7ae1eb4e2498d7
ahmedatk <ahmedzaweel@gmail.com>
2025-03-26 05:31:26 +0530

    pyrebase

--- CST/notes/views.py
+++ CST/notes/views.py
@@ -9,6 +9,23 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from .models import Note
 from .forms import NoteForm
+import pyrebase
+
+
+config={
+    "apiKey": "AIzaSyC5h8gEZbusQ8PN89Da-5HJpSRDNtEmgL4",
+    "authDomain": "cstnotes-f0e59.firebaseapp.com",
+    "databaseURL": "https://cstnotes-f0e59-default-rtdb.firebaseio.com",
+    "projectId": "cstnotes-f0e59",
+    "storageBucket": "cstnotes-f0e59.firebasestorage.app",
+    "messagingSenderId": "17433439165",
+    "appId": "1:17433439165:web:70213cd44c15c4ea2b40d4",
+    "measurementId": "G-5FGDKSLYGY"
+}
+
+firebase = pyrebase.initialize_app(config)
+authe=firebase.auth()
+database=firebase.database()
 
 def health_check(request):
     return HttpResponse("OK")
